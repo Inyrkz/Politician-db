c3b4fc0c4355d089f74314df49445572e72736f0
Ini-Abasi Bernard <55382743+Inyrkz@users.noreply.github.com>
2019-12-03 09:39:29 +0100

    Add New Files
    Updated dataset, updated codes, added flask_restful to flask app

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
-from flask import Flask, request, jsonify
+'''flask restful api'''
 import json
-import pandas as pd
+from flask import Flask, request, jsonify
+from flask_restful import Api
 from politician import get_recommendations
 
 app = Flask(__name__)
+api = Api(app)
 
 @app.route('/', methods=['POST'])
 def predict():
+    '''function under post request'''
     #Get data from Post request
-    data =  request.json.get('Name')
+    data = request.json.get('Name')
+    #fit input data into get recommendation function
     pred = get_recommendations(data)
     return jsonify({"Predictions": pred})
     #return pred.to_json()
 
+
 if __name__ == "__main__":
-   app.run(debug=True)
+    app.run(debug=True)
    
